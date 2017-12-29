51ff91d987e60083a6e6e89f91017c508feafe13
K Web <kenweb2011@gmail.com>
2017-12-29 17:37:13 -0600

    webpack mocha

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,9 +1,22 @@
-var express = require('express');
-var path = require('path');
-var open = require('open');
+import express from 'express';
+import path from 'path';
+//import open from 'open';
+import webpack from 'webpack';
+import config from '../webpack.config.dev';
+//var express = require('express');
+//var path = require('path');
+//var open = require('open');
 
-var port = 3000;
-var app = express();
+/* eslint-disable no-console */
+
+const port = 3000;
+const app = express();
+const compiler = webpack(config);
+
+app.use(require('webpack-dev-middleware')(compiler, {
+    noInfo: true,
+    publicPath: config.output.publicPath
+}));
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, '../src/index.html'));
@@ -13,6 +26,7 @@ app.listen(port, function(err) {
     if (err) {
         console.log(err);
     } else {
-        open('http://localhost:' + port);
+        console.log('Running...');
+  //      open('http://localhost:3000'); // + port);
     }
 });
